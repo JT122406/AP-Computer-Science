a7c646b03566bd7181ffc5b6333cadd5d5de9624
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2021-11-29 18:53:40 -0500

    Added CSP project files

---
+++ GPA Calculator/GPA Calculator/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ApplicationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeveloperInfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBoxGrade = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonNext = New System.Windows.Forms.Button()
+        Me.ButtonCalculate = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ApplicationToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(260, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ApplicationToolStripMenuItem
+        '
+        Me.ApplicationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.InfoToolStripMenuItem, Me.DeveloperInfoToolStripMenuItem})
+        Me.ApplicationToolStripMenuItem.Name = "ApplicationToolStripMenuItem"
+        Me.ApplicationToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
+        Me.ApplicationToolStripMenuItem.Text = "Application"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InfoToolStripMenuItem.Text = "About"
+        '
+        'DeveloperInfoToolStripMenuItem
+        '
+        Me.DeveloperInfoToolStripMenuItem.Name = "DeveloperInfoToolStripMenuItem"
+        Me.DeveloperInfoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DeveloperInfoToolStripMenuItem.Text = "Developer info"
+        '
+        'TextBoxGrade
+        '
+        Me.TextBoxGrade.Location = New System.Drawing.Point(16, 144)
+        Me.TextBoxGrade.Name = "TextBoxGrade"
+        Me.TextBoxGrade.Size = New System.Drawing.Size(108, 20)
+        Me.TextBoxGrade.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Enter numberic grade:"
+        '
+        'ButtonNext
+        '
+        Me.ButtonNext.Location = New System.Drawing.Point(16, 170)
+        Me.ButtonNext.Name = "ButtonNext"
+        Me.ButtonNext.Size = New System.Drawing.Size(108, 23)
+        Me.ButtonNext.TabIndex = 9
+        Me.ButtonNext.Text = "Next Class"
+        Me.ButtonNext.UseVisualStyleBackColor = True
+        '
+        'ButtonCalculate
+        '
+        Me.ButtonCalculate.Location = New System.Drawing.Point(138, 170)
+        Me.ButtonCalculate.Name = "ButtonCalculate"
+        Me.ButtonCalculate.Size = New System.Drawing.Size(110, 23)
+        Me.ButtonCalculate.TabIndex = 10
+        Me.ButtonCalculate.Text = "Calculate"
+        Me.ButtonCalculate.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(108, 98)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Class Weight"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(6, 75)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(78, 17)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "AP/IB 1.25"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(6, 55)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(100, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Honors/101 1.2"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(6, 37)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(79, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "A-Level 1.1"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 19)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(79, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "B-Level 1.0"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton7)
+        Me.GroupBox2.Controls.Add(Me.RadioButton6)
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Location = New System.Drawing.Point(131, 28)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(117, 76)
+        Me.GroupBox2.TabIndex = 12
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Credits"
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Location = New System.Drawing.Point(7, 54)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(75, 17)
+        Me.RadioButton7.TabIndex = 2
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "2.5 Credits"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(7, 36)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButton6.TabIndex = 1
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "5 Credits"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(7, 18)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(72, 17)
+        Me.RadioButton5.TabIndex = 0
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "10 Credits"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(260, 202)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ButtonCalculate)
+        Me.Controls.Add(Me.ButtonNext)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxGrade)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "GPA Calculator"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ApplicationToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DeveloperInfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TextBoxGrade As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ButtonNext As System.Windows.Forms.Button
+    Friend WithEvents ButtonCalculate As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton7 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ GPA Calculator/GPA Calculator/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Dim totalCredits As Double
+    Dim GPAS As Double
+    Private Sub InfoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InfoToolStripMenuItem.Click
+        MessageBox.Show("GPA Calculator uses a normal 4.0 scale from the College board website. It multiplies the weight of the class, amount of credits, and grade point of the grade you got in each class adds them up for all the classes then divides by the total amount of credits to get total GPA https://pages.collegeboard.org/how-to-convert-gpa-4.0-scale")
+    End Sub
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub ButtonNext_Click(sender As Object, e As EventArgs) Handles ButtonNext.Click
+        Dim weight As Double = 0
+        Dim grade As Integer
+        If (Me.TextBoxGrade.Text = Nothing) Or Not (Me.RadioButton1.Checked Or Me.RadioButton2.Checked Or Me.RadioButton3.Checked Or Me.RadioButton4.Checked) Or Not (Me.RadioButton5.Checked Or Me.RadioButton6.Checked Or Me.RadioButton7.Checked) Then
+            grade = -1
+        Else
+            grade = Val(Me.TextBoxGrade.Text)
+        End If
+        If grade <= 100 And grade >= 0 Then
+            Dim gradepoint As Double = 0
+            Dim Credits As Double
+            If RadioButton1.Checked Then
+                weight = 1
+            ElseIf RadioButton2.Checked Then
+                weight = 1.1
+            ElseIf RadioButton3.Checked Then
+                weight = 1.2
+            ElseIf RadioButton4.Checked Then
+                weight = 1.25
+            End If
+
+            If RadioButton5.Checked Then
+                Credits = 10
+            ElseIf RadioButton6.Checked Then
+                Credits = 5
+            ElseIf RadioButton7.Checked Then
+                Credits = 2.5
+            End If
+
+            totalCredits = totalCredits + Credits
+            If grade >= 94 Then
+                gradepoint = 4.0
+            ElseIf grade >= 90 Then
+                gradepoint = 3.7
+            ElseIf grade >= 87 Then
+                gradepoint = 3.3
+            ElseIf grade >= 84 Then
+                gradepoint = 3.0
+            ElseIf grade >= 80 Then
+                gradepoint = 2.7
+            ElseIf grade >= 77 Then
+                gradepoint = 2.3
+            ElseIf grade >= 74 Then
+                gradepoint = 2.0
+            ElseIf grade >= 70 Then
+                gradepoint = 1.7
+            ElseIf grade >= 67 Then
+                gradepoint = 1.3
+            ElseIf grade >= 64 Then
+                gradepoint = 1.0
+            ElseIf grade >= 60 Then
+                gradepoint = 0.7
+            Else
+                gradepoint = 0
+            End If
+            Dim GPA As Double = (gradepoint * weight * Credits)
+            GPAS = GPAS + GPA
+            Me.TextBoxGrade.Clear()
+        Else
+            MessageBox.Show("Enter a valid Grade, Weight, and Credits")
+        End If
+    End Sub
+    Private Sub ButtonCalculate_Click(sender As Object, e As EventArgs) Handles ButtonCalculate.Click
+        Dim finalGPA As Double = GPAS / totalCredits
+        MessageBox.Show("Your cumulative GPA is: " + Str(finalGPA))
+        GPAS = 0
+        totalCredits = 0
+    End Sub
+
+    Private Sub DeveloperInfoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeveloperInfoToolStripMenuItem.Click
+        MessageBox.Show("Hi")
+    End Sub
+End Class
